c298d73497cbd55fcba2ed3553e618448e3adf3e
Yuliannahernandez <yulianahernandezc01@gmail.com>
2025-12-07 01:32:07 -0600

    sucursales

--- src/sucursales/sucursales.module.ts
+++ src/sucursales/sucursales.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { SucursalesController } from './sucursales.controller';
 import { SucursalesService } from './sucursales.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule ,HttpModule,],
   controllers: [SucursalesController],
   providers: [SucursalesService],
+  
   exports: [SucursalesService],
 })
 export class SucursalesModule {}
